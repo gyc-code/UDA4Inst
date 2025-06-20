99eb1ce4a059631384351fa3177f750e629e9b10
Yachan Guo <947177904@qq.com>
2025-06-20 12:07:54 +0800

    Update train_loop.py

--- detectron2/detectron2/engine/train_loop.py
+++ detectron2/detectron2/engine/train_loop.py
@@ -621,7 +621,7 @@ class AMPTrainer(SimpleTrainer):
                     else:
                         t2s_mix_losses = sum(t2s_mix_loss_dict.values())
                     # unite_loss = 0.5 * source_losses + 0.25 * t2s_mix_losses + 0.25 * s2t_mix_losses
-                    unite_loss = 0.25 * t2s_mix_losses + 0.75 * s2t_mix_losses
+                    unite_loss = 0.6 * t2s_mix_losses + 0.4 * s2t_mix_losses
                     unite_loss_dict = t2s_mix_loss_dict
                 else:
                     ''' use mini batch loss ,one batch data=source+s2t+t2s ''' 
